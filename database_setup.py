c8b63108b2d49b85b6e82eba486bb95f5b712cc3
CruzanCaramele <letters2hamza@gmail.com>
2015-03-10 16:26:01 +0800

    Add column for university location

--- database_setup.py
+++ database_setup.py
@@ -33,6 +33,7 @@ class University(Base):
 	#column definitions for the university table
 	id = Column(Integer, primary_key=True)
 	name = Column(String(250), nullable=False)
+	city = Column(String(80), nullable=False)
 
 
 
