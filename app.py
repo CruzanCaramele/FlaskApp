662cb1e5e6e77bf6f71f92b8ef9409891a9baef5
CruzanCaramele <letters2hamza@gmail.com>
2015-04-28 16:43:18 +0800

    grant access to users to add and edit rooms to universities created by others but only such users can perform CRUD ops on such rooms

--- app.py
+++ app.py
@@ -45,13 +45,14 @@ session=DBSession()
 
 
 
-@app.route("/login", methods=["GET", "POST"])
+#Create anti-forgery state token
+@app.route('/login')
 def showLogin():
-	#anti-forgery state token	
-	state = "".join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
-	login_session["state"] = state
-	#return "The current session state is %s" % login_session["state"]
-	return render_template("login.html", STATE=state)
+
+  	state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+  	login_session['state'] = state
+  	#return "The current session state is %s" % login_session['state']
+  	return render_template('login.html', STATE = state)
 
 
 @app.route('/gconnect', methods=['POST'])
@@ -61,11 +62,13 @@ def gconnect():
     		response = make_response(json.dumps('Invalid state parameter.'), 401)
     		response.headers['Content-Type'] = 'application/json'
     		return response
-  	#Obtain authorization code
-  	code = request.data
-  
+  	#Obtain authorization code, now compatible with Python3
+  	request.get_data()
+  	code = request.data.decode('utf-8')
+
   	try:
-   		# Upgrade the authorization code into a credentials object
+
+    		# Upgrade the authorization code into a credentials object
     		oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
     		oauth_flow.redirect_uri = 'postmessage'
     		credentials = oauth_flow.step2_exchange(code)
@@ -76,18 +79,22 @@ def gconnect():
   
   	# Check that the access token is valid.
   	access_token = credentials.access_token
-  	url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+  	url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+         	% access_token)
+  	# Submit request, parse response - Python3 compatible 
   	h = httplib2.Http()
-  	result = json.loads(h.request(url, 'GET')[1])
+  	response = h.request(url, 'GET')[1]
+  	str_response = response.decode('utf-8')
+  	result = json.loads(str_response)
+
   	# If there was an error in the access token info, abort.
   	if result.get('error') is not None:
     		response = make_response(json.dumps(result.get('error')), 500)
     		response.headers['Content-Type'] = 'application/json'
 
-    
- 	# Verify that the access token is used for the intended user.
+  	# Verify that the access token is used for the intended user.
   	gplus_id = credentials.id_token['sub']
- 	if result['user_id'] != gplus_id:
+  	if result['user_id'] != gplus_id:
     		response = make_response(json.dumps("Token's user ID doesn't match given user ID."), 401)
     		response.headers['Content-Type'] = 'application/json'
     		return response
@@ -95,25 +102,24 @@ def gconnect():
   	# Verify that the access token is valid for this app.
   	if result['issued_to'] != CLIENT_ID:
     		response = make_response(json.dumps("Token's client ID does not match app's."), 401)
-    		print "Token's client ID does not match app's."
     		response.headers['Content-Type'] = 'application/json'
     		return response
 
-  	stored_credentials = login_session.get('credentials')
+  	stored_access_token = login_session.get('access_token')
   	stored_gplus_id = login_session.get('gplus_id')
-  	if stored_credentials is not None and gplus_id == stored_gplus_id:
-   		response = make_response(json.dumps('Current user is already connected.'), 200)
+  	if stored_access_token is not None and gplus_id == stored_gplus_id:
+    		response = make_response(json.dumps('Current user is already connected.'),200)
     		response.headers['Content-Type'] = 'application/json'
     		return response
     
   	# Store the access token in the session for later use.
-  	login_session['credentials'] = credentials.access_token
+  	login_session['access_token'] = access_token
   	login_session['gplus_id'] = gplus_id
  
   
   	#Get user info
   	userinfo_url =  "https://www.googleapis.com/oauth2/v1/userinfo"
-  	params = {'access_token': credentials.access_token, 'alt':'json'}
+  	params = {'access_token': access_token, 'alt':'json'}
   	answer = requests.get(userinfo_url, params=params)
   
   	data = answer.json()
@@ -121,12 +127,13 @@ def gconnect():
   	login_session['username'] = data['name']
   	login_session['picture'] = data['picture']
   	login_session['email'] = data['email']
-
-  	#check if user exits
-  	user_id = getUserID(login_session["email"])
+ 
+  	#see if user exists, if it doesn't make a new one
+  	user_id = getUserID(login_session['email'])
   	if not user_id:
-  		user_id = createUser(login_session)
-  	login_session["user_id"] = user_id
+    		user_id = createUser(login_session)
+  	login_session['user_id'] = user_id
+
 
   	output = ''
   	output +='<h1>Welcome, '
@@ -136,11 +143,11 @@ def gconnect():
   	output += login_session['picture']
   	output +=' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
   	flash("you are now logged in as %s"%login_session['username'])
-  	print "done!"
   	return output
 
-#helper functions and creating user
+#User Helper Functions
 def createUser(login_session):
+
   	newUser = User(name = login_session['username'], email = login_session['email'], picture = login_session['picture'])
   	session.add(newUser)
   	session.commit()
@@ -158,30 +165,29 @@ def getUserID(email):
   	except:
       		return None
 
+#DISCONNECT - Revoke a current user's token and reset their login_session
 @app.route('/gdisconnect')
 def gdisconnect():
 
     	#Only disconnect a connected user.
-  	credentials = login_session.get('credentials')
-  	if credentials is None:
-    		response = make_response(json.dumps('Current user not connected.'),401)
+  	access_token = login_session.get('access_token')
+  	if access_token is None:
+   		response = make_response(json.dumps('Current user not connected.'),401)
     		response.headers['Content-Type'] = 'application/json'
     		return response 
-  	access_token = login_session['credentials']
   	url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
   	h = httplib2.Http()
   	result = h.request(url, 'GET')[0]
-
   	if result['status'] == '200':
     		#Reset the user's sesson.
-    		del login_session['credentials']
+    		del login_session['access_token']
     		del login_session['gplus_id']
     		del login_session['username']
     		del login_session['email']
     		del login_session['picture']
 
     		response = make_response(json.dumps('Successfully disconnected.'), 200)
-    		response.headers['Content-Type'] = 'application/json'
+   		response.headers['Content-Type'] = 'application/json'
     		return response
   	else:
     		# For whatever reason, the given token was invalid.
@@ -305,7 +311,7 @@ def newRoom(university_id):
 		aNewRoom = Room(owner_name=request.form["ownerName"], size=request.form["roomSize"]\
 				       , picture=filename, description=request.form["roomDescription"], price=request.form["roomPrice"]\
 				        , address=request.form["adress"], owner_number=request.form["phoneNum"], \
-				        university_id=university_id, user_id=room.user_id)
+				        university_id=university_id, user_id=login_session["user_id"])
 
 		session.add(aNewRoom)
 		session.commit()
@@ -328,7 +334,7 @@ def editRoom(university_id, room_id):
 	roomToEdit = session.query(Room).filter_by(id=room_id).one()	
 	university = session.query(University).filter_by(id=university_id).one()
 
-	if login_session["user_id"] != university.user_id:
+	if login_session["user_id"] != roomToEdit .user_id:
 		return "<script>function myFunction() {alert('You are not authorized to edit this room. Please create your own in order to edit items.');}</script><body onload='myFunction()''>"
 
 	if request.method == "POST":
@@ -337,7 +343,7 @@ def editRoom(university_id, room_id):
 		if request.form["roomSize"]:
 			roomToEdit.size = request.form["roomSize"]
 		if request.form["roomDescription"]:
-			roomToEdit.description = request.fom["roomDescription"]
+			roomToEdit.description = request.form["roomDescription"]
 		if request.form["roomPrice"]:
 			roomToEdit.price = request.form["roomPrice"]
 		if request.form["adress"]:
@@ -367,8 +373,8 @@ def deleteRoom(university_id, room_id):
 
 	roomToDelete = session.query(Room).filter_by(id=room_id).one()
 	university = session.query(University).filter_by(id=university_id).one()
-
-	if login_session["user_id"] != university.user_id:
+	room_poster = getUserID(login_session["email"])
+	if login_session["user_id"] != roomToDelete.user_id:
 		return "<script>function myFunction() {alert('You are not authorized to delete this room.');}</script><body onload='myFunction()''>"
 
 	if request.method == "POST":
